5a3dd1e72452cebc77a43eb89af5ce60c78ebbe2
Victor Liang <victor.zw.liang@gmail.com>
2020-06-02 19:04:47 +0000

    Added typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,26 @@
+const sentence = "hello there from lighthouse labs";
+
+const delayPrint = function(sentence) {
+  let time = 0;
+  for (const char of sentence) {
+    setTimeout(() => {
+
+      process.stdout.write(char);
+
+    }, time); 
+
+    time += 50;
+  }
+  setTimeout(() => {
+    console.log('\n');
+  }, time);
+};
+
+delayPrint(sentence);
+
+
+
+
+
+
+
